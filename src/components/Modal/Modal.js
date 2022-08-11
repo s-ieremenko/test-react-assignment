e49956d0e768442e608b2e63b2ce2daf9146329b
s-ieremenko <ieremenko.sveta@gmail.com>
2022-08-11 14:10:12 +0200

    changed styles

--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -1,4 +1,5 @@
 import React from 'react'
+
 import styles from './Modal.module.css'
 
 const Modal = (props) => {
@@ -10,6 +11,7 @@ const Modal = (props) => {
         customerIdentificationCode,
         handleCloseModal,
     } = props
+
     return (
         <div className={styles.modalBackground}>
             <div className={styles.modalContainer}>
